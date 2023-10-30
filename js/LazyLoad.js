1637b12915bd76ec24bf6e3731281b30ae39919e
ElTigre001kz <121248984+ElTigre001kz@users.noreply.github.com>
2023-10-31 01:43:34 +0600

    Update LazyLoad.js

--- js/LazyLoad.js
+++ js/LazyLoad.js
@@ -1,9 +1,29 @@
-var lazyLoadInstance = new LazyLoad({
-  window.addEventListener('scroll', function () {
-    // Загружаем контейнеры, когда они видимы на экране
-    lazyLoadInstance.update();
+// Инициализация LazyLoad
+const lazyLoadInstance = new LazyLoad({
+  elements_selector: ".lazy", // Выбираем элементы с классом "lazy"
+  threshold: 0, // Загружаем элементы, когда они полностью видимы на экране
+  callback_enter: function(element) {
+    // Колбэк, вызываемый при появлении элемента на экране
+    element.classList.add("loaded"); // Добавляем класс "loaded" для анимации или других стилей
+  },
+  callback_exit: function(element) {
+    // Колбэк, вызываемый при скрытии элемента за пределами экрана
+    element.classList.remove("loaded"); // Удаляем класс "loaded"
+  }
 });
-});
+
+// // Обработчик события прокрутки страницы
+// window.addEventListener('scroll', function () {
+//   // Загружаем контейнеры, когда они видимы на экране
+//   lazyLoadInstance.update();
+// });
+
+// var lazyLoadInstance = new LazyLoad({
+//   window.addEventListener('scroll', function () {
+//     // Загружаем контейнеры, когда они видимы на экране
+//     lazyLoadInstance.update();
+// });
+// });
 
 
 // Инициализация LazyLoad.js
