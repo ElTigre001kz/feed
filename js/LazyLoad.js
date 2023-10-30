75e146372f23306cf3f63676aed5c8138eaf931b
ElTigre001kz <121248984+ElTigre001kz@users.noreply.github.com>
2023-10-31 01:37:21 +0600

    Update LazyLoad.js

--- js/LazyLoad.js
+++ js/LazyLoad.js
@@ -1,8 +1,15 @@
-// Инициализация LazyLoad.js
-const lazyLoadInstance = new LazyLoadInstance();
-
-// Обработчик события прокрутки страницы
-window.addEventListener('scroll', function () {
+var lazyLoadInstance = new LazyLoad({
+  window.addEventListener('scroll', function () {
     // Загружаем контейнеры, когда они видимы на экране
     lazyLoadInstance.update();
 });
+
+
+// Инициализация LazyLoad.js
+// const lazyLoadInstance = new LazyLoadInstance();
+
+// // Обработчик события прокрутки страницы
+// window.addEventListener('scroll', function () {
+//     // Загружаем контейнеры, когда они видимы на экране
+//     lazyLoadInstance.update();
+// });
