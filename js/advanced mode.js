43f9dff08a14b102632cb9581c63560ae83c20f7
ElTigre001kz <121248984+ElTigre001kz@users.noreply.github.com>
2023-11-01 14:42:53 +0600

    Update advanced mode.js

--- js/advanced mode.js
+++ js/advanced mode.js
@@ -1,6 +1,38 @@
+// function togglePostSize(post) {
+//         post.classList.toggle('expanded');
+//         const posts = document.querySelectorAll('.post');
+//         const posterTitles = document.querySelectorAll('.poster-title');
 
+//         for (const otherPost of posts) {
+//             if (otherPost !== post) {
+//                 otherPost.classList.toggle('expanded');
+//             }
+//         }
 
+//         for (const title of posterTitles) {
+//             if (post.classList.contains('expanded')) {
+//                 title.style.display = "none";
+//             } else {
+//                 title.style.display = "flex";
+//             }
+//         }
 
+//         if (post.classList.contains('expanded')) {
+//             window.scrollTo(0, post.offsetTop);
+//             history.pushState({ expandedPost: true }, "");
+//         } else {
+//             history.pushState({ expandedPost: false }, "");
+//         }
+//     }
+
+//     window.addEventListener("popstate", function(e) {
+//         if (e.state && e.state.expandedPost) {
+//             const expandedPost = document.querySelector('.post.expanded');
+//             if (expandedPost) {
+//                 togglePostSize(expandedPost);
+//             }
+//         }
+//     });
 
 
 
@@ -25,46 +57,8 @@
 
         if (post.classList.contains('expanded')) {
             window.scrollTo(0, post.offsetTop);
-            // history.pushState({ expandedPost: true }, "");
-        } else {
-            // history.pushState({ expandedPost: false }, "");
         }
     }
-
-    window.addEventListener("popstate", function(e) {
-        if (e.state && e.state.expandedPost) {
-            const expandedPost = document.querySelector('.post.expanded');
-            if (expandedPost) {
-                togglePostSize(expandedPost);
-            }
-        }
-    });
-
-
-
-    // function togglePostSize(post) {
-    //     post.classList.toggle('expanded');
-    //     const posts = document.querySelectorAll('.post');
-    //     const posterTitles = document.querySelectorAll('.poster-title');
-
-    //     for (const otherPost of posts) {
-    //         if (otherPost !== post) {
-    //             otherPost.classList.toggle('expanded');
-    //         }
-    //     }
-
-    //     for (const title of posterTitles) {
-    //         if (post.classList.contains('expanded')) {
-    //             title.style.display = "none";
-    //         } else {
-    //             title.style.display = "flex";
-    //         }
-    //     }
-
-    //     if (post.classList.contains('expanded')) {
-    //         window.scrollTo(0, post.offsetTop);
-    //     }
-    // }
 
 
  // <script>
