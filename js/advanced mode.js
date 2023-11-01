7ddfbcdd4ec6f41ddff9563f2c30296da1117aa9
ElTigre001kz <121248984+ElTigre001kz@users.noreply.github.com>
2023-11-01 14:53:29 +0600

    Update advanced mode.js

--- js/advanced mode.js
+++ js/advanced mode.js
@@ -1,38 +1,78 @@
 function togglePostSize(post) {
-        post.classList.toggle('expanded');
-        const posts = document.querySelectorAll('.post');
-        const posterTitles = document.querySelectorAll('.poster-title');
+    post.classList.toggle('expanded');
+    const posts = document.querySelectorAll('.post');
+    const posterTitles = document.querySelectorAll('.poster-title');
 
-        for (const otherPost of posts) {
-            if (otherPost !== post) {
-                otherPost.classList.toggle('expanded');
-            }
-        }
-
-        for (const title of posterTitles) {
-            if (post.classList.contains('expanded')) {
-                title.style.display = "none";
-            } else {
-                title.style.display = "flex";
-            }
-        }
-
-        if (post.classList.contains('expanded')) {
-            window.scrollTo(0, post.offsetTop);
-            history.pushState({ expandedPost: true }, "");
-        } else {
-            history.pushState({ expandedPost: false }, "");
+    for (const otherPost of posts) {
+        if (otherPost !== post) {
+            otherPost.classList.toggle('expanded');
         }
     }
 
-    window.addEventListener("popstate", function(e) {
-        if (e.state && e.state.expandedPost) {
-            const expandedPost = document.querySelector('.post.expanded');
-            if (expandedPost) {
-                togglePostSize(expandedPost);
-            }
+    for (const title of posterTitles) {
+        if (post.classList.contains('expanded')) {
+            title.style.display = "none";
+        } else {
+            title.style.display = "flex";
         }
-    });
+    }
+
+    if (post.classList.contains('expanded')) {
+        post.scrollIntoView({ behavior: 'smooth' });
+        history.pushState({ expandedPost: true }, "");
+    } else {
+        history.pushState({ expandedPost: false }, "");
+    }
+}
+
+window.addEventListener("popstate", function(e) {
+    if (e.state && e.state.expandedPost) {
+        const expandedPost = document.querySelector('.post.expanded');
+        if (expandedPost) {
+            togglePostSize(expandedPost);
+        }
+    }
+});
+
+
+
+
+
+// function togglePostSize(post) {
+//         post.classList.toggle('expanded');
+//         const posts = document.querySelectorAll('.post');
+//         const posterTitles = document.querySelectorAll('.poster-title');
+
+//         for (const otherPost of posts) {
+//             if (otherPost !== post) {
+//                 otherPost.classList.toggle('expanded');
+//             }
+//         }
+
+//         for (const title of posterTitles) {
+//             if (post.classList.contains('expanded')) {
+//                 title.style.display = "none";
+//             } else {
+//                 title.style.display = "flex";
+//             }
+//         }
+
+//         if (post.classList.contains('expanded')) {
+//             window.scrollTo(0, post.offsetTop);
+//             history.pushState({ expandedPost: true }, "");
+//         } else {
+//             history.pushState({ expandedPost: false }, "");
+//         }
+//     }
+
+//     window.addEventListener("popstate", function(e) {
+//         if (e.state && e.state.expandedPost) {
+//             const expandedPost = document.querySelector('.post.expanded');
+//             if (expandedPost) {
+//                 togglePostSize(expandedPost);
+//             }
+//         }
+//     });
 
 
 
